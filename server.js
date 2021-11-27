8a9cffa4ba25a87a0f2a3791e6b085133cea61d4
Fangchen Chen <chenfc@live.unc.edu>
2021-11-27 18:33:19 -0500

    ??

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get("/app/", (req, res, next) => {
 
 // Define other CRUD API endpoints using express.js and better-sqlite3
 // CREATE a new user (HTTP method POST) at endpoint /app/new/
-app.post("/app/users", (req, res) => {	
+app.post("/app/new/user", (req, res) => {	
 	const stmt = db.prepare("INSERT INTO userinfo (user, pass) VALUES (?,?)").run();
 	res.status(200).json(stmt);
 });
@@ -35,18 +35,18 @@ app.get("/app/users", (req, res) => {
 });
 
 // READ a single user (HTTP method GET) at endpoint /app/user/:id
-app.get("/app/users", (req, res) => {	
+app.get("/app/user/:id", (req, res) => {	
 	const stmt = db.prepare("SELECT * FROM userinfo WHERE id = 2").get();
 	res.status(200).json(stmt);
 });
 
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
-app.post("/app/users", (req, res) => {	
+app.patch("/app/update/user/:id", (req, res) => {	
 	const stmt = db.prepare("UPDATE userinfo SET user=COALESCE(?,user), pass = COALESCE(?,pass) WHERE id=?").run();
 	res.status(200).json(stmt);
 });
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
-app.post("/app/users", (req, res) => {	
+app.delete("/app/delete/user/:id", (req, res) => {	
 	const stmt = db.prepare("DELETE FROM userinfo WHERE id=?").run();
 	res.status(200).json(stmt);
 });
