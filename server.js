26fb8c4f31304664ccce83fc975775a778bf8d80
Fangchen Chen <chenfc@live.unc.edu>
2021-11-27 18:29:44 -0500

    ?

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 var HTTP_PORT = 5000;
 // Start server
 app.listen(HTTP_PORT, () => {
-    console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
+    console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT));
 });
 // READ (HTTP method GET) at root endpoint /app/
 app.get("/app/", (req, res, next) => {
@@ -24,7 +24,10 @@ app.get("/app/", (req, res, next) => {
 
 // Define other CRUD API endpoints using express.js and better-sqlite3
 // CREATE a new user (HTTP method POST) at endpoint /app/new/
-
+app.post("/app/users", (req, res) => {	
+	const stmt = db.prepare("INSERT INTO userinfo (user, pass) VALUES (?,?)").run();
+	res.status(200).json(stmt);
+});
 // READ a list of all users (HTTP method GET) at endpoint /app/users/
 app.get("/app/users", (req, res) => {	
 	const stmt = db.prepare("SELECT * FROM userinfo").all();
@@ -32,11 +35,21 @@ app.get("/app/users", (req, res) => {
 });
 
 // READ a single user (HTTP method GET) at endpoint /app/user/:id
+app.get("/app/users", (req, res) => {	
+	const stmt = db.prepare("SELECT * FROM userinfo WHERE id = 2").get();
+	res.status(200).json(stmt);
+});
 
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
-
+app.post("/app/users", (req, res) => {	
+	const stmt = db.prepare("UPDATE userinfo SET user=COALESCE(?,user), pass = COALESCE(?,pass) WHERE id=?").run();
+	res.status(200).json(stmt);
+});
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
-
+app.post("/app/users", (req, res) => {	
+	const stmt = db.prepare("DELETE FROM userinfo WHERE id=?").run();
+	res.status(200).json(stmt);
+});
 // Default response for any other request
 app.use(function(req, res){
 	res.json({"message":"Endpoint not found. (404)"});
