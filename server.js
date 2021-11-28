04ea255384bcdf29bcd5282ec9891f84fa7038ad
Fangchen Chen <chenfc@live.unc.edu>
2021-11-27 19:42:13 -0500

    .

--- server.js
+++ server.js
@@ -13,45 +13,51 @@ app.use(express.json());
 // Set server port
 var HTTP_PORT = 5000;
 // Start server
-app.listen(HTTP_PORT, () => {
-    console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT));
+const server = app.listen(HTTP_PORT, () => {
+	console.log("Server running on port %PORT%".replace("%PORT%", HTTP_PORT));
 });
 // READ (HTTP method GET) at root endpoint /app/
 app.get("/app/", (req, res, next) => {
-    res.json({"message":"Your API works! (200)"});
+	res.json({ "message": "Your API works! (200)" });
 	res.status(200);
 });
 
 // Define other CRUD API endpoints using express.js and better-sqlite3
 // CREATE a new user (HTTP method POST) at endpoint /app/new/
-app.post("/app/new/", (req, res) => {	
-	const stmt = db.prepare("INSERT INTO userinfo (user, md5(pass)) VALUES (?,?)").run();
+app.post("/app/new/", (req, res) => {
+	const stmt = db.prepare("INSERT INTO userinfo (user, pass) VALUES (?,?)").run(req.body.user, md5(req.body.pass));
 	res.status(201).json(stmt);
 });
 // READ a list of all users (HTTP method GET) at endpoint /app/users/
-app.get("/app/users/", (req, res) => {	
+app.get("/app/users/", (req, res) => {
 	const stmt = db.prepare("SELECT * FROM userinfo").all();
 	res.status(200).json(stmt);
 });
 
 // READ a single user (HTTP method GET) at endpoint /app/user/:id
-app.get("/app/user/:id", (req, res) => {	
-	const stmt = db.prepare("SELECT * FROM userinfo WHERE id = 2").get();
+app.get("/app/user/:id", (req, res) => {
+	const stmt = db.prepare("SELECT * FROM userinfo WHERE id = ?").get(req.body.id);
 	res.status(200).json(stmt);
 });
 
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
-app.patch("/app/update/user/:id", (req, res) => {	
-	const stmt = db.prepare("UPDATE userinfo SET user=COALESCE(?,user), pass = md5(COALESCE(?,pass)) WHERE id=?").run();
+app.patch("/app/update/user/:id", (req, res) => {
+	const stmt = db.prepare("UPDATE userinfo SET user=COALESCE(?,user), pass = COALESCE(?,pass) WHERE id=?").run(req.body.user, md5(req.body.pass), req.body.id);
 	res.status(200).json(stmt);
 });
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
-app.delete("/app/delete/user/:id", (req, res) => {	
-	const stmt = db.prepare("DELETE FROM userinfo WHERE id=?").run();
+app.delete("/app/delete/user/:id", (req, res) => {
+	const stmt = db.prepare("DELETE FROM userinfo WHERE id=?").run(req.body.id);
 	res.status(200).json(stmt);
 });
 // Default response for any other request
-app.use(function(req, res){
-	res.json({"message":"Endpoint not found. (404)"});
-    res.status(404);
+app.use(function (req, res) {
+	res.json({ "message": "Endpoint not found. (404)" });
+	res.status(404);
+});
+// Tell STDOUT that the server is stopped
+process.on('SIGTERM', () => {
+	server.close(() => {
+		console.log('Server stopped.');
+	});
 });
--- test.js
+++ test.js
@@ -1,6 +1,18 @@
 var db = require('./database.js');
 var md5 = require("md5");
 
-const all= db.prepare('SELECT * FROM userinfo').all();
+all= db.prepare('SELECT * FROM userinfo').all();
+
+function updateall(){
+    all = db.prepare('SELECT * FROM userinfo').all();
+}
+
 console.log("\nAll records in my database:", db.name);
-console.log(all)
+console.log(all);
+
+const getuser = db.prepare('SELECT * FROM userinfo WHERE id = ?');
+const output = getuser.get('1');
+
+
+console.log("\n Full record for username", output.user);
+console.log(output)
